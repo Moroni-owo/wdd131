50b9aa8c0443c145fa0f12b2ee9f8d40c5c0aa24
Moroni Capcha <raulmoronicapchacadillo@gmail.com>
2025-12-02 10:03:00 -0500

    week6

---
+++ scripts/footer.js
@@ -0,0 +1,6 @@
+// Set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modification date
+document.getElementById("lastModified").textContent =
+  "Last Modification: " + document.lastModified;
---
+++ scripts/review.js
@@ -0,0 +1,7 @@
+// Counter using localStorage
+let count = Number(localStorage.getItem("reviewCount")) || 0;
+count++;
+localStorage.setItem("reviewCount", count);
+
+document.getElementById("counter").textContent =
+  `You have submitted ${count} reviews.`;
